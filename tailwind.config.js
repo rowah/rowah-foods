7dcb0175514db61dd7612f8fb1fb74fe659c450d
Rowa <jrowah@gmail.com>
2023-05-18 15:22:40 +0300

    import fonts from google

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        mammoth: "0.5rem",
+      },
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "E2E2D5",
+          200: "888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
